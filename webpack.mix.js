6b6f95625469db08289e684340c6e58c51a04d82
tejas <tejastg007@gmail.com>
2021-09-27 00:11:15 +0530

    included purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
 /*
  |--------------------------------------------------------------------------
@@ -15,4 +16,5 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .styles('resources/css/app.css','public/css/custom.css')
     .styles('resources/css/home.css','public/css/home.css')
+    .purgeCss()
     .sourceMaps();
